e61973f5ea4096c6fb92b2263a0934f659b08ae9
nath54 <cerisara.nathan@gmail.com>
2026-01-17 19:31:57 +0100

    Nathan: debug

--- src/main.py
+++ src/main.py
@@ -7,9 +7,9 @@ from src.main_play import play
 from src.main_interactive import interactive
 
 #
-def parse_args() -> argparse.NameSpace:
+def parse_args() -> argparse.Namespace:
     #
-    parser = argparse.ArgumentParser(description="Robot Corridor RL Training")
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description="Robot Corridor RL Training")
     #
     parser.add_argument('--config', type=str, default='config/main.yaml', help='Config file path')
     parser.add_argument('--train', action='store_true', help='Train the agent')
@@ -20,7 +20,7 @@ def parse_args() -> argparse.NameSpace:
     parser.add_argument('--model_path', type=str, default=None, help='Model path for play mode')
     parser.add_argument('--live_vision', action='store_true', help='Show live vision window')
     #
-    args = parser.parse_args()
+    args: argparse.Namespace = parser.parse_args()
     #
     return args
 
@@ -29,37 +29,48 @@ def parse_args() -> argparse.NameSpace:
 def main() -> None:
 
     # Parse arguments
-    args = parse_args()
+    args: argparse.Namespace = parse_args()
 
     # Launch the selected mode:
 
     # Run training pipeline of training runs
     if args.pipeline:
+        #
         from src.main_pipeline import run_pipeline
+        #
         run_pipeline(args.pipeline)
 
     # Run single training run
     elif args.train:
+        #
         cfg = load_config(args.config)
+        #
         try:
             mp.set_start_method('spawn', force=True)
         except RuntimeError:
             pass
+        #
         train(cfg)
 
     # Agents play with trained model so we can see what they learned
     elif args.play:
+        #
         cfg = load_config(args.config)
+        #
         model_path = args.model_path if args.model_path else cfg.training.model_path
+        #
         play(cfg, model_path, args.live_vision)
 
     # Interactive keyboard control
     elif args.interactive:
+        #
         cfg = load_config(args.config)
+        #
         interactive(cfg, args.render_mode)
 
     # Invalid mode
     else:
+        #
         print("Please specify mode: --train, --play, --interactive, or --pipeline")
 
 #
